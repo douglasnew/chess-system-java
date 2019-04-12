2f967d6d1177c277e4d78272bd4c3d94dbff01fb
Douglas Neumann <donascimento@fh.com.br>
2019-04-11 21:09:44 -0300

    Project created

---
+++ src/application/Program.java
@@ -0,0 +1,27 @@
+/*
+ * COPYRIGHT FH 2019 - ALL RIGHTS RESERVED.
+ * 
+ * This software is only to be used for the purpose for which it has been
+ * provided. No part of it is to be reproduced, disassembled, transmitted,
+ * stored in a retrieval system nor translated in any human or computer
+ * language in any way or for any other purposes whatsoever without the prior
+ * written consent of FH.
+ */
+package application;
+
+
+/**
+ * @author FH - donascimento
+ *
+ */
+public class Program {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World!");
+    }
+
+}
